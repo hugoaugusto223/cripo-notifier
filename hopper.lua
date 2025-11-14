70c8fff8258d3267f37b4469eeb333d607fe1aab
windpeak <bielferrari2021@gmail.com>
2025-11-13 22:02:37 -0300

    Update hopper.lua

--- hopper.lua
+++ hopper.lua
@@ -1,16 +1,12 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
 repeat task.wait() until game:GetService("Players").LocalPlayer
 
-if type(setfpscap) == "function" then
-	setfpscap(15)
-else
-	warn("setfpscap não está disponível nesse executor")
-end
+setfpscap(15)
 
 
 local CONFIG = {
 	API = {
-		BASE_URL = "https://notifier-production-081a.up.railway.app/",
+		BASE_URL = "https://notifier-production-081a.up.railway.app",
 		ADD_ENDPOINT = "/add",
 		GET_JOB_ENDPOINT = "/get-job",
 		TOKEN = "Qe4pVJZy7Wn82Xs0bL6tCFAiR3S9dUEq"
@@ -18,71 +14,7 @@ local CONFIG = {
 	SEARCH = {
 		MIN_GENERATION = 0,
 		 BRAINROT_NAMES = {
-    "Noobini Pizzanini", "Lirilì Larilà", "Tim Cheese", "Fluriflura",
-    "Svinina Bombardino", "Talpa Di Fero", "Pipi Kiwi", "Pipi Corni",
-    "Raccooni Jandelini", "Tartaragno", "Trippi Troppi", "Gangster Footera",
-    "Boneca Ambalabu", "Ta Ta Ta Ta Sahur", "Tric Trac Baraboom", "Bandito Bobritto",
-    "Cacto Hipopotamo", "Pipi Avocado", "Pinealotto Fruttarino", "Cupcake Koala",
-    "Cappuccino Assassino", "Brr Brr Patapim", "Trulimero Trulicina", "Bananita Dolphinita",
-    "Brri Brri Bicus Dicus Bombicus", "Bambini Crostini", "Perochello Lemonchello", "Avocadini Guffo",
-    "Salamino Penguino", "Ti Ti Ti Sahur", "Penguino Cocosino", "Avocadini Antilopini",
-    "Bandito Axolito", "Malame Amarele", "Mangolini Parrocini", "Mummio Rappitto",
-    "Frogato Pirato", "Wombo Rollo", "Doi Doi Do", "Burbaloni Loliloli",
-    "Chimpanzini Bananini", "Ballerina Cappuccina", "Chef Crabracadabra", "Glorbo Fruttodrillo",
-    "Blueberrinni Octopusini", "Lionel Cactuseli", "Pandaccini Bananini", "Strawberrelli Flamingelli",
-    "Cocosini Mama", "Pi Pi Watermelon", "Sigma Boy", "Pipi Potato",
-    "Quivioli Ameleonni", "Tirilikalika Tirilikalako", "Caramello Filtrello", "Signore Carapace",
-    "Sigma Girl", "Quackula", "Buho de Fuego", "Clickerino Crabo",
-    "Frigo Camelo", "Orangutini Ananassini", "Bombardiro Crocodilo", "Bombombini Gusini",
-    "Rhino Toasterino", "Cavallo Virtuoso", "Spioniro Golubiro", "Zibra Zubra Zibralini",
-    "Tigrilini Watermelini", "Gorillo Watermelondrillo", "Avocadorilla", "Ganganzelli Trulala",
-    "Tob Tobi Tobi", "Te Te Te Sahur", "Tracoducotulu Delapeladustuz", "Lerulerulerule",
-    "Carloo", "Carrotini Brainini", "Brutto Gialutto", "Gorillo Subwoofero",
-    "Los Noobinis", "Rhino Helicopterino", "Elefanto Frigo", "Toiletto Focaccino",
-    "Cachorrito Melonito", "Bananito Bandito", "Magi Ribbitini", "Jacko Spaventosa",
-    "Stoppo Luminino", "Chihuanini Taconini", "Cocofanto Elefanto", "Tralalero Tralala",
-    "Odin Din Din Dun", "Girafa Celestre", "Trenostruzzo Turbo 3000", "Matteo",
-    "Tigroligre Frutonni", "Orcalero Orcala", "Unclito Samito", "Gattatino Nyanino",
-    "Espresso Signora", "Ballerino Lololo", "Piccione Macchina", "Los Crocodillitos",
-    "Tukanno Bananno", "Trippi Troppi Troppa Trippa", "Los Tungtungtungcitos", "Agarrini la Palini",
-    "Bulbito Bandito Traktorito", "Los Orcalitos", "Tipi Topi Taco", "Bombardini Tortinii",
-    "Tralalita Tralala", "Urubini Flamenguini", "Alessio", "Pakrahmatmamat",
-    "Los Bombinitos", "Brr es Teh Patipum", "Tartaruga Cisterna", "Cacasito Satalito",
-    "Mastodontico Telepiedone", "Crabbo Limonetta", "Gattito Tacoto", "Los Tipi Tacos",
-    "Antonio", "Las Capuchinas", "Orcalita Orcala", "Piccionetta Macchina",
-    "Anpali Babel", "Extinct Ballerina", "Tractoro Dinosauro", "Belula Beluga",
-    "Capi Taco", "Dug dug dug", "Corn Corn Corn Sahur", "Brasilini Berimbini",
-    "Squalanana", "Pop Pop Sahur", "Vampira Cappucina", "Jacko Jack Jack",
-    "Snailenzo", "Tentacolo Tecnico", "Pakrahmatmatina", "Bambu Bambu Sahur",
-    "Krupuk Pagi Pagi", "Mummy Ambalabu", "Cappuccino Clownino", "Skull Skull Skull",
-    "Aquanaut", "Frio Ninja", "Money Money Man", "Noo La Polizia",
-    "La Vacca Saturno Saturnita", "Los Tralaleritos", "Graipuss Medussi", "La Grande Combinasion",
-    "Sammyni Spyderini", "Garama and Madundung", "Torrtuginni Dragonfrutini", "Las Tralaleritas",
-    "Pot Hotspot", "Nuclearo Dinossauro", "Las Vaquitas Saturnitas", "Chicleteira Bicicleteira",
-    "Agarrini la Palini", "Los Combinasionas", "Karkerkar Kurkur", "Dragon Cannelloni",
-    "Los Hotspotsitos", "Esok Sekolah", "Nooo My Hotspot", "Los Matteos",
-    "Job Job Job Sahur", "Dul Dul Dul", "Blackhole Goat", "Los Spyderinis",
-    "Ketupat Kepat", "La Supreme Combinasion", "Bisonte Giuppitere", "Guerriro Digitale",
-    "Ketchuru and Musturu", "Spaghetti Tualetti", "Los Nooo My Hotspotsitos", "Trenostruzzo Turbo 4000",
-    "Fragola La La La", "La Sahur Combinasion", "La Karkerkar Combinasion", "Tralaledon",
-    "Los Bros", "Los Chicleteiras", "Chachechi", "Extinct Tralalero",
-    "Extinct Matteo", "67", "Las Sis", "Celularcini Viciosini",
-    "La Extinct Grande", "Quesadilla Crocodila", "Tacorita Bicicleta", "La Cucaracha",
-    "To to to Sahur", "Mariachi Corazoni", "Los Tacoritas", "Tictac Sahur",
-    "Yess my examine", "Karker Sahur", "Noo my examine", "Money Money Puggy",
-    "Los Primos", "Tang Tang Keletang", "Perrito Burrito", "Chillin Chili",
-    "Los Tortus", "Los Karkeritos", "Los Jobcitos", "Los 67",
-    "La Secret Combinasion", "Burguro And Fryuro", "Zombie Tralala", "Vulturino Skeletono",
-    "Frankentteo", "La Vacca Jacko Linterino", "Chicleteirina Bicicleteirina", "Eviledon",
-    "La Spooky Grande", "Los Mobilis", "Spooky and Pumpky", "Boatito Auratito",
-    "Horegini Boom", "Rang Ring Bus", "Mieteteira Bicicleteira", "Quesadillo Vampiro",
-    "Burrito Bandito", "Chipso and Queso", "Jackorilla", "Pumpkini Spyderini",
-    "Trickolino", "Telemorte", "Pot Pumpkin", "Noo my Candy",
-    "Los Spooky Combinasionas", "La Casa Boo", "Headless Horseman", "La Taco Combinasion",
-    "1x1x1x1", "Capitano Moby", "Guest 666", "Pirulitoita Bicicleteira",
-    "Los Puggies", "Los Spaghettis", "Fragrama and Chocrama", "Strawberry Elephant",
-    "Meowl", "Mythic Lucky Block", "Brainrot God Lucky Block", "Secret Lucky Block",
-    "Admin Lucky Block", "Taco Lucky Block", "Los Lucky Blocks", "Spooky Lucky Block"
+"Lucky Block", "La Vacca Saturno Saturnita", "Karkerkar Kurkur", "Los Matteos", "Bisonte Giuppitere", "Trenostruzzo Turbo 4000", "Jackorilla", "Sammyni Spyderini", "Torrtuginni Dragonfrutini", "Dul Dul Dul", "Blackhole Goat", "Chachechi", "Agarrini la Palini", "Los Spyderinis", "Fragola La La La", "Extinct Tralalero", "La Cucaracha", "Los Tralaleritos", "Los Tortus", "Zombie Tralala", "Vulturino Skeletono", "Boatito Auratito", "Guerriro Digitale", "Yess my examine", "La Karkerkar Combinasion", "Extinct Matteo", "Las Tralaleritas", "Pumpkini Spyderini", "Job Job Job Sahur", "Frankentteo", "Karker Sahur", "Las Vaquitas Saturnitas", "Los Karkeritos", "La Vacca Jacko Linterino", "Trickolino", "Graipuss Medussi", "Perrito Burrito", "1x1x1x1", "Nooo My Hotspot", "Los Jobcitos", "Noo my examine", "La Sahur Combinasion", "Telemorte", "To to to Sahur", "Pot Hotspot", "Pirulitoita Bicicleteira", "Horegini Boom", "Quesadilla Crocodila", "Pot Pumpkin", "Chicleteira Bicicleteira", "Quesadillo Vampiro", "Chicleteirina Bicicleteirina", "Burrito Bandito", "Noo my Candy", "Los Nooo My Hotspotsitos", "Rang Ring Bus", "Guest 666", "Los Chicleteiras", "67", "La Grande Combinasion", "Mariachi Corazoni", "Nuclearo Dinossauro", "Los Combinasionas", "Tacorita Bicicleta", "Las Sis", "Los Hotspotsitos", "Los Spooky Combinasionas", "Money Money Puggy", "Los Mobilis", "Celularcini Viciosini", "Los 67", "La Extinct Grande", "Los Bros", "La Spooky Grande", "Chillin Chili", "Chipso and Queso", "Mieteteira Bicicleteira", "Tralaledon", "Esok Sekolah", "Los Puggies", "Los Primos", "Eviledon", "Los Tacoritas", "Tang Tang Keletang", "Ketupat Kepat", "La Taco Combinasion", "Tictac Sahur", "La Supreme Combinasion", "Ketchuru and Musturu", "Garama and Madundung", "Spaghetti Tualetti", "Los Spaghettis", "Spooky and Pumpky", "La Casa Boo", "Fragrama and Chocrama", "La Secret Combinasion", "Burguro And Fryuro", "Capitano Moby", "Headless Horseman", "Dragon Cannelloni", "Meowl", "Strawberry Elephant"
 },
 
 		EXCLUDED_NAMES = {"craft", "fusing", "ready"},
@@ -131,8 +63,7 @@ end
 
 
 local function getHttpRequestFunction()
-	return http_request or request or (syn and syn.request)
-		or (fluxus and fluxus.request) or (http and http.request)
+	return http_request or request or (syn and syn.request) or (fluxus and fluxus.request) or (http and http.request)
 end
 
 local function makeRequest(cfg)
@@ -205,7 +136,7 @@ local function sendPetNotification(name, generation, jobId, uniqueId)
 	local payload = {
 		name = name,
 		generation = generation,
-		job_id = jobId, 
+		job_id = jobId,
 		players = #Players:GetPlayers() .. "/8",
 		timer = os.date("%d/%m/%Y %H:%M:%S")
 	}
@@ -220,13 +151,12 @@ local function sendPetNotification(name, generation, jobId, uniqueId)
 		Body = HttpService:JSONEncode(payload)
 	})
 
-	if res and (res.StatusCode or res.status or 0) < 300 then
-		log("success", "✓ " .. name .. " [" .. generation .. "]")
+	if res and res.StatusCode and res.StatusCode >= 200 and res.StatusCode < 300 then
 		return true
 	end
+
 	return false
 end
-
 
 local function collectPet(overhead, genText, displayText)
 	local value = parseValue(genText)
@@ -242,49 +172,67 @@ local function collectPet(overhead, genText, displayText)
 end
 
 local function scanPlots()
-    local pets = {}
-    local now = tick()
+	local pets = {}
+	local now = tick()
 
-    
-    for _, obj in pairs(Workspace:GetDescendants()) do
-        if obj.Name == "AnimalOverhead" and (obj:IsA("BillboardGui") or obj:IsA("SurfaceGui")) then
-            local gen = obj:FindFirstChild("Generation")
-            local disp = obj:FindFirstChild("DisplayName")
-            if gen and disp and gen:IsA("TextLabel") and disp:IsA("TextLabel") then
-                local genText, dispText = gen.Text, disp.Text
-                if genText ~= "" and dispText ~= "" then
-                    local val = parseValue(genText)
-                    if isBrainrotPet(dispText) and val >= CONFIG.SEARCH.MIN_GENERATION then
-                        local key = string.format("%s_%s_%s_%s", dispText, genText, game.JobId, obj:GetFullName())
-                        if not petCache[key] or (now - petCache[key]) >= CONFIG.NETWORK.CACHE_DURATION then
-                            petCache[key] = now
-                            table.insert(pets, {
-                                name = dispText,
-                                generation = genText,
-                                value = val,
-                                uniqueId = obj:GetFullName()
-                            })
-                        end
-                    end
-                end
-            end
-        end
-    end
+	for _, obj in ipairs(Workspace:GetDescendants()) do
+		if obj.Name == "AnimalOverhead" and (obj:IsA("BillboardGui") or obj:IsA("SurfaceGui")) then
+			local gen = obj:FindFirstChild("Generation")
+			local disp = obj:FindFirstChild("DisplayName")
 
- 
-    local sent = 0
-    for _, pet in ipairs(pets) do
-        task.spawn(function()
-            local s = sendPetNotification(pet.name, pet.generation, game.JobId, pet.uniqueId)
-            if s >= 200 and s < 300 then
-                sent += 1
-            end
-        end)
-    end
+			if gen and disp then
+				local genText = gen.Text
+				local dispText = disp.Text
+				local val = parseValue(genText)
 
-    return sent
+				if genText ~= "" and dispText ~= ""
+					and isBrainrotPet(dispText)
+					and val >= CONFIG.SEARCH.MIN_GENERATION
+				then
+					local key = dispText .. "_" .. genText .. "_" .. obj:GetFullName()
+					if not petCache[key] or (now - petCache[key]) >= CONFIG.NETWORK.CACHE_DURATION then
+						petCache[key] = now
+						table.insert(pets, {
+							name = dispText,
+							generation = genText,
+							uniqueId = obj:GetFullName()
+						})
+					end
+				end
+			end
+		end
+	end
+
+	return pets
 end
 
+local function sendAllWithRetry(pets)
+	if #pets == 0 then return true end
+
+	local failed = {}
+
+	for _, pet in ipairs(pets) do
+		local ok = sendPetNotification(pet.name, pet.generation, game.JobId, pet.uniqueId)
+		if not ok then
+			table.insert(failed, pet)
+		end
+	end
+
+	if #failed == 0 then return true end
+
+	task.wait(5)
+
+	local failedAgain = {}
+
+	for _, pet in ipairs(failed) do
+		local ok = sendPetNotification(pet.name, pet.generation, game.JobId, pet.uniqueId)
+		if not ok then
+			table.insert(failedAgain, pet)
+		end
+	end
+
+	return #failedAgain == 0
+end
 
 local HttpService = game:GetService("HttpService")
 local TeleportService = game:GetService("TeleportService")
@@ -399,12 +347,25 @@ local function main()
 	log("info", "iniciando scan")
 	setupGraphics()
 
-	local found = safeCall(scanPlots, "erro no scan") or 0
-	if found > 0 then
-		log("info", "1s antes de hoppar")
-		task.wait(1)
+	local pets = safeCall(scanPlots, "erro no scan") or {}
+
+	if #pets == 0 then
+		log("info", "nenhum pet encontrado")
+		safeCall(serverHop, "erro no server hop")
+		return
 	end
-	safeCall(serverHop, "erro no server hop")
+
+	local allOk = safeCall(function()
+		return sendAllWithRetry(pets)
+	end, "erro ao enviar requests")
+
+	if allOk then
+		log("success", "todos funcionou")
+		task.wait(1)
+		safeCall(serverHop, "erro no server hop")
+	else
+		log("warn", "alguns falhou mesmo após retry")
+	end
 end
 
 main()
